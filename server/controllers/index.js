f654ab1b58c5655eee97429d2a26bf905b837261
elderny <eldernydeveloper@gmail.com>
2022-06-18 19:07:00 +0530

    part 10

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 const addProducts = require('./admin/addProducts')
+const getProducts = require('./products/getProducts')
 
 // Post requests
 router.post('/addProducts', addProducts)
+
+// Get requests
+router.get('/getProducts',getProducts)
+
 module.exports = router
