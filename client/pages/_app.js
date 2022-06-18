63d899c332bfaccb756902053659925538e37911
elderny <eldernydeveloper@gmail.com>
2022-06-18 10:38:06 +0530

    part 5

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,9 @@
+import Footer from '../components/Footer'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Footer />
+  </>
 }
